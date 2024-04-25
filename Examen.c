c8c137ab15eaebf98a7b8f4668de1ea42adddc9c
Mexi <161762841+EmilioMaximiliano@users.noreply.github.com>
2024-04-25 14:05:19 -0600

    Add files via upload

---
+++ Examen.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+    int 400600600043.50408;
+    int IVA:
+    int impuesto;
+    int 20%
+    
+    printf("Thanos para los siguentes años habra ganado: ");
+    {
+    printf("Año 1:")
+    res = 400600600043.50408 - impuesto;
+    cout<<400600600043.50408<<" - "<<20%<<" = "<<res;
+    }
+    
+    {
+    printf("Año 2:");
+    res = 800120120087.00816 - impuesto;
+    cout<<400600600043.50408<<" - "<<20%<<" = "<<res;
+    }
+    
+     {
+    printf("Año 3:");
+    res = 1602402400174.016 - impuesto;
+    cout<<1602402400174.016<<" - "<<20%<<" = "<<res;
+    }
+    
+     {
+    printf("Año 4:");
+    res = 2204804800348.032 - impuesto - IVA;
+    cout<<2204804800348.032<<" - "<<20%<<" - "<<10%<<" = "<<res;
+    }
+    return 0;
+    
+     {
+    printf("Año 5:");
+    res = 2609609600696.064 - impuesto - IVA;
+    cout<<2609609600696.064<<" - "<<20%<<" - "<<10%<<" = "<<res;
+    }
+    return 0
+}
